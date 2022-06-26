fc4e29dee70fb72a25cacb608d2d3e945a8cb5c3
Johnny <joaogardenberg@gmail.com>
2022-06-26 14:11:13 +0200

    useMemo on background

--- src/common/Background.jsx
+++ src/common/Background.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, useMemo } from 'react'
 import { TextureLoader, WebGLCubeRenderTarget } from 'three'
 import { useLoader, useThree } from '@react-three/fiber'
 
@@ -6,9 +6,13 @@ export default function Background() {
   const { gl } = useThree()
   const texture = useLoader(TextureLoader, '/autoshop.jpg')
 
-  const formatted = new WebGLCubeRenderTarget(
-    texture.image.height
-  ).fromEquirectangularTexture(gl, texture)
+  const formatted = useMemo(
+    () =>
+      new WebGLCubeRenderTarget(
+        texture.image.height
+      ).fromEquirectangularTexture(gl, texture),
+    [gl, texture]
+  )
 
   return (
     <Suspense fallback={null}>
